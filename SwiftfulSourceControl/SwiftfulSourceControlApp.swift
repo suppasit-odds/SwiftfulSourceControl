32e1363b2619ce5bac8f6d56c393b09f3fda8fee
Suppasit <monitorman_sc@hotmail.com>
2024-06-17 15:03:09 +0700

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Suppasit chuwatsawat on 17/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
